0ffdb9fd77f9c9a3d2e36cda27799976be75e792
khansharikh <sknkkhan47@gmail.com>
2023-06-21 07:27:27 -0400

    commite from local

---
+++ springboot-demo/src/test/java/com/star/agile/assignment/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,9 @@
+package com.star.agile.assignment.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+	
+}
